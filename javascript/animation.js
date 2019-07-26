3e2b0c0ad251104474e2518ffc40fbf632ebc1bf
Charlie BOYER <charlieboyer@outlook.fr>
2019-07-26 02:37:02 +0200

    Add a fex JavaScript lines for animations events, without success for the rainbow-border
    Note : Also remove empty and meaningless javascript.js

---
+++ javascript/animation.js
@@ -0,0 +1,16 @@
+/*
+* JavaScript for index.html
+* Manage icons animation inside the #skills block
+*/
+
+function setRainbowBorder() {
+    document.getElementsByClassName(".project_block").setAttribute("class", "project_block rainbow");
+}
+ 
+function returnToBlueBorder() {
+
+}
+
+function returnToPurpleBorder() {
+
+}
